1943b0f5b46dd736ec69a84a29f35288dc0057a3
Sergiy <44747525+Tsurkanovskiy@users.noreply.github.com>
2020-04-19 15:19:00 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE match (
     match_id  NUMBER(4) NOT NULL,
     match_date  DATE NOT NULL,
-    participant1_id  NUMBER(4) NOT NULL
+    participant1_id  NUMBER(4) NOT NULL,
     participant2_id  NUMBER(4) NOT NULL
 );
 
@@ -11,7 +11,9 @@ CREATE TABLE participant (
     participant_id  NUMBER(4) NOT NULL,
     player_name  CHAR(14) NOT NULL,
     faction CHAR(2) NOT NULL,
-    victory_status CHAR(8) NOT NULL
+    victory_status CHAR(8) NOT NULL,
+    CHECK (faction IN ('P','Z','T')),
+    CHECK (victory_status IN ('[winner]','[loser]'))
 );
 
 ALTER TABLE participant ADD CONSTRAINT participant_pk PRIMARY KEY ( participant_id );
@@ -38,9 +40,3 @@ ALTER TABLE participant
     ADD CONSTRAINT participant_name_fk FOREIGN KEY ( player_name )
         REFERENCES player ( player_name )
     NOT DEFERRABLE;
-
-ALTER TABLE participant
-ADD CONSTRAINT factions CHECK (faction faction IN ('P','Z','T') ) [DISABLE];
-
-ALTER TABLE participant
-ADD CONSTRAINT won_or_lost CHECK (victory_status victory_status IN ('[winner]','[loser]') ) [DISABLE];
